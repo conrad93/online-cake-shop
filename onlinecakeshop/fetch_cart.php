9660d3c263294b2369c2a8858d1f412d9803379f
conrad93 <64744613+conrad93@users.noreply.github.com>
2022-07-12 10:18:54 +0530

    Update fetch_cart.php
    add to cart issue

--- onlinecakeshop/fetch_cart.php
+++ onlinecakeshop/fetch_cart.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
 $id = $_GET['id'];
-if (!in_array($id, $_SESSION['cart'])) {
+if(empty($_SESSION['cart'])){
 	$_SESSION['cart'][] = $id;
+} else {
+	if (!in_array($id, $_SESSION['cart'])) {
+		$_SESSION['cart'][] = $id;
+	}
 }
 echo json_encode($_SESSION['cart']);
 ?>
